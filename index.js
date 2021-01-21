6736f15d84a8aa109b1fec3afd5465309c1b4743
Nandini Proothi <46650119+nandiniproothi@users.noreply.github.com>
2021-01-21 23:17:27 +0530

    v0.2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let requestURL = 'https://saurav.tech/NewsAPI/top-headlines/category/health/in.json';
+var url;
+let requestURL = 'https://saurav.tech/NewsAPI/everything/cnn.json';
 let request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
@@ -7,4 +8,13 @@ request.send();
 request.onload = function() {
   const res = request.response;
   console.log(res);
+  const num = Math.floor((Math.random() * res.articles.length));
+  console.log(num);
+  url = res.articles[num].url;
+}
+
+function externalWeb(){
+    console.log("clicked");
+    window.open(url);
+    window.location.reload()
 }
